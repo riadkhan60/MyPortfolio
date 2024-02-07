f6a0bbe1b53b5bfc884a0588879797494bbe6551
riadkhan60 <khanriad60@gmail.com>
2024-02-08 05:07:20 +0600

    menu toggle added

--- src/features/DrawerMenu/NavDrawer.jsx
+++ src/features/DrawerMenu/NavDrawer.jsx
@@ -1,7 +1,10 @@
+import { useGSAP } from "@gsap/react";
 import useMenuContext from "../../contexts/useMenuContext";
 import NavFooter from "./NavFooter";
 import NavList from "./NavList";
 import { TfiClose } from "react-icons/tfi";
+import { useRef } from "react";
+import gsap from "gsap";
 
 const navList = [
   "Home",
@@ -15,17 +18,20 @@ const navList = [
 
 function NavDrawer() {
   const { onCloseMenu } = useMenuContext();
+
+  const container = useRef(null);
   return (
     <div
+      ref={container}
       id="drawer"
-      className="fixed right-0 top-0 z-50 md:h-[130vh] h-full bg-[#181715] transition-all duration-500 ease-in-out max-lg:left-0 lg:right-0 lg:w-[44%]"
+      className="fixed right-0 top-0 z-50 max-md:h-[130vh] h-full bg-[#181715] transition-all duration-500 ease-in-out max-lg:left-0 lg:right-0 lg:w-[44%]"
     >
       <div onClick={onCloseMenu} className=" group absolute right-[40px] top-[40px] flex h-10 w-10 items-center justify-center border-subtitleColor hover:border ">
         <div className="  cursor-pointer text-4xl text-themeDarkshade transition-all duration-300 ease-in-out group-hover:text-xl group-hover:text-subtitleColor  ">
           <TfiClose />
         </div>
       </div>
-      <div className="flex h-full w-full flex-col justify-between px-[16%] py-[100px] max-sm:px-5">
+      <div id="op" className="flex h-full w-full flex-col justify-between px-[16%] py-[100px] max-sm:px-5">
         <NavList navList={navList} />
         <NavFooter />
       </div>
