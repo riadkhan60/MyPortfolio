12eeb21a748c7fb4f44028d11f11bca8ec8ac3ec
riadkhan60 <khanriad60@gmail.com>
2024-02-08 04:32:49 +0600

    menu toggle added

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import useScrollAnimation from "./hooks/useScrollAnimation";
 
 function App() {
   useScrollAnimation();
-  
+
   return (
     <MenuProvider>
       <div className=" h-full w-[100%] transition-all duration-0 ease-in-out max-lg:block">
         <Profile />
         <div id="content" className="lg:w-[56%]">
+          <Home />
           <Home />
         </div>
         <MenuLayout />
