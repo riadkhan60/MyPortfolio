17518567482b84a7007d62e88c1d2e9a07c04685
riadkhan60 <khanriad60@gmail.com>
2024-02-13 03:55:55 +0600

    typo fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import MouseBall from './Ui/MouseBAll.jsx'
+import MouseBall from './Ui/MouseBall.jsx'
 
 
 
