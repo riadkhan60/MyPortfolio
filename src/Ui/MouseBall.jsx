00182e9a6672ed598dcdf1a46b00c84d1662151e
riadkhan60 <khanriad60@gmail.com>
2024-02-13 04:09:30 +0600

    animation elements hide in small displays

--- src/Ui/MouseBall.jsx
+++ src/Ui/MouseBall.jsx
@@ -14,19 +14,19 @@ function MouseBall() {
       positionMouseInnerY: 0,
       speedOuter: 6,
       speedInner: 2,
-      cursorStyle: 'normal'
+      cursorStyle: "normal",
     };
     const handleMouseMove = (event) => {
       settings.mouseX = event.clientX;
       settings.mouseY = event.clientY;
 
-        if (window.getComputedStyle(event.target).cursor === "pointer") {
-          console.log("Cursor is a pointer");
-          settings.cursorStyle = "pointer";
-        } else {
-          console.log("Cursor is not a pointer");
-          settings.cursorStyle = "normal";
-        }
+      if (window.getComputedStyle(event.target).cursor === "pointer") {
+        console.log("Cursor is a pointer");
+        settings.cursorStyle = "pointer";
+      } else {
+        console.log("Cursor is not a pointer");
+        settings.cursorStyle = "normal";
+      }
     };
 
     const updatePosition = () => {
@@ -34,21 +34,18 @@ function MouseBall() {
         (settings.mouseX - settings.positionMouseOuterX) / settings.speedOuter;
       settings.positionMouseOuterY +=
         (settings.mouseY - settings.positionMouseOuterY) / settings.speedOuter;
-      
+
       settings.positionMouseInnerX +=
         (settings.mouseX - settings.positionMouseInnerX) / settings.speedInner;
       settings.positionMouseInnerY +=
         (settings.mouseY - settings.positionMouseInnerY) / settings.speedInner;
-      
-    
-      
-      if (settings.cursorStyle === 'pointer') {
-        mouseBallRef.current.classList.add('pointer');
-        mouseBallSmallRef.current.classList.add('pointer');
+
+      if (settings.cursorStyle === "pointer") {
+        mouseBallRef.current.classList.add("pointer");
+        mouseBallSmallRef.current.classList.add("pointer");
       } else {
-        mouseBallRef.current.classList.remove('pointer');
-        mouseBallSmallRef.current.classList.remove('pointer');
-        
+        mouseBallRef.current.classList.remove("pointer");
+        mouseBallSmallRef.current.classList.remove("pointer");
       }
 
       mouseBallRef.current.style.transform = `translateX(${settings.positionMouseOuterX - mouseBallRef.current.offsetWidth / 2}px) translateY(${settings.positionMouseOuterY - mouseBallRef.current.offsetHeight / 2}px)`;
@@ -67,14 +64,14 @@ function MouseBall() {
       <div
         id="mouseBall"
         ref={mouseBallRef}
-        className=" pointer-events-none fixed left-0 top-0 z-[10000]  "
+        className=" pointer-events-none fixed left-0 top-0 z-[10000] max-md:hidden  "
       >
-        <span className="block h-[30px] w-[30px] rounded-full border-2 border-[rgba(255,255,255,0.5)] transition-all duration-300"></span>
+        <span className=" block h-[30px] w-[30px] rounded-full border-2 border-[rgba(255,255,255,0.5)] transition-all duration-300"></span>
       </div>
       <div
         id="mouseBallSmall"
         ref={mouseBallSmallRef}
-        className=" pointer-events-none fixed left-0 top-0 z-[10000]  "
+        className=" pointer-events-none fixed left-0 top-0 z-[10000] max-md:hidden  "
       >
         <span className="block h-[6px] w-[6px] rounded-full  bg-[rgba(255,255,255,0.5)] transition-all duration-300"></span>
       </div>
