105955b80da970263698c4dd1f6b80214504d1ad
riadkhan60 <khanriad60@gmail.com>
2024-05-16 03:58:23 +0600

    Modal added for portfolios

--- src/Ui/SectionContainer.jsx
+++ src/Ui/SectionContainer.jsx
@@ -1,6 +1,6 @@
 function SectionContainer({ children }) {
   return (
-    <div className=" max-md:mb-[150px] flex lg:min-h-dvh items-center">
+    <div className=" max-lg:mb-[150px] flex lg:min-h-dvh items-center">
       <div className="px-[15px] max-md:mt-2 w-full sm:px-[20px] lg:px-[12%]">{children}</div>
     </div>
   );
